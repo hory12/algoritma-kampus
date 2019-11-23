eee2bde69a18822e5abaaba8b7d8dd3cd743b290
hory12 <horyradiansyah5@gmail.com>
2019-11-23 07:57:16 +0700

    pindah_folder

---
+++ Latihan 1/ifbercabang.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int angka;
+	cout<<"MASUKAN JUMLAH NILAI:";
+	cin>> angka;
+	if (angka>=85){
+		cout<<"nilai=A"<<endl;
+	}else if (angka>=75){
+		cout<< "nilai=B +"<<endl;
+			}else if (angka>=65){
+		cout<< "nilai=C +"<<endl;
+			}else if (angka = 45){
+		cout<< "nilai=C "<<endl;
+			}else if (angka>=30){
+		cout<< "nilai=D +"<<endl;
+			}else if (angka>=20){
+		cout<< "nilai=D "<<endl;
+			}else if (angka>=0){
+		cout<< "nilai=E "<<endl;
+	}
+}
