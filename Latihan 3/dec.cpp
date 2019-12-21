3bb027116109c55b5271bfe9a0a489d659abf04f
hory12 <horyradiansyah5@gmail.com>
2019-12-21 08:15:07 +0700

    laihan3

---
+++ Latihan 3/dec.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     int MD;
+
+     for(MD=8;MD>0;MD--){
+          cout<<MD<<endl;
+     }
+
+     return 0;
+}
+
---
+++ Latihan 3/inc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     int MD;
+
+     for(MD=0;MD<8;MD++){
+          cout<<"1"<<endl;
+     }
+
+     return 0;
+}
+
